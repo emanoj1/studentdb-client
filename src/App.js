ab9bb2a7669f0937deb3e2c17cf53ef7dd0d593f
Manoj Kumar <37259245+emanoj1@users.noreply.github.com>
2024-07-14 16:17:37 +1000

    Set up Routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import logo from './assets/images/studentDB-logo.png';
-import './App.css';
-import Registration from './components/Registration';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Login from './components/Login';
+import Registration from './components/Registration';
+import Dashboard from './components/Dashboard';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Registration />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+
+
