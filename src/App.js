5d2b626655186545232117365057ab1519eb698e
Manoj Kumar <37259245+emanoj1@users.noreply.github.com>
2024-07-14 15:46:27 +1000

    Initial setup Client side

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './assets/images/studentDB-logo.png';
+import './App.css';
+import Registration from './components/Registration';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      
+    </div>
+  );
+}
+
+export default App;
