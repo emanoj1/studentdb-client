43968438f322a95254062e5a3de726ad96faff84
Manoj Kumar <37259245+emanoj1@users.noreply.github.com>
2024-07-24 18:22:12 +1000

    Remove unnecessary commenting

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
-import './Dashboard.css'; // Import the CSS file
+import './Dashboard.css';
 
 function Dashboard() {
   const [statistics, setStatistics] = useState({
