4dc0e0f8bb850a61a00a10356f63e0797e8dedba
Manoj Kumar <37259245+emanoj1@users.noreply.github.com>
2024-08-02 12:05:02 +1000

    feat: implement Tally contact form

---
+++ src/components/Contact.js
@@ -0,0 +1,33 @@
+// This is the Contact page using the Tally.so widget
+
+import React, { useEffect } from 'react';
+import './Contact.css';
+
+function Contact() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://tally.so/widgets/embed.js';
+    script.async = true;
+    script.onload = () => {
+      if (window.Tally) {
+        window.Tally.loadEmbeds();
+      }
+    };
+    document.head.appendChild(script);
+  }, []);
+
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <iframe
+        data-tally-src="https://tally.so/embed/mDejJZ?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
+        loading="lazy"
+        height="563"
+        title="Contact Us (StudentDB)"
+        className="tally-form"
+      ></iframe>
+    </div>
+  );
+}
+
+export default Contact;
